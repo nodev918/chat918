2086156f94ccbb75dbde7900159df956ba8af3ae
Node HSU(徐俊彥) <G-nodehsu@gamania.com>
2021-08-31 14:57:59 +0800

    aa

--- app.js
+++ app.js
@@ -57,5 +57,5 @@ io.on('connection', (socket)=>{
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT,()=>{
-    console.log(`listeningggg: ${PORT}`)
+    console.log(`listening: ${PORT}`)
 })
